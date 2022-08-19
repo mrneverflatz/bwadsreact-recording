47a07286d25a3c4cc57d454f8e1cb579f9f95362
Yein Narayana <mrneverflatz@gmail.com>
2022-08-19 21:56:51 +0800

    add theme

---
+++ .storybook/laracamp.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "BWADSREACT",
+  brandUrl: "https://bwadsreact-recording.netlify.app/",
+  brandImage: "/logo.png",
+  brandTarget: "_self",
+});
